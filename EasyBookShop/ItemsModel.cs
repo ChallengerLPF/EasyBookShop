4bea5b814b8a8d38817b4d59bf10f01803666b0f
Lahiru P Fernando <Lahiru P Fernando>
2017-03-08 00:21:42 +0530

    mvc trying with items first day finish

---
+++ EasyBookShop/ItemsModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyBookShop
+{
+    class ItemsModel
+    {
+        int id;
+        string barcode;
+        string description;
+        string category;
+        string sub_category;
+        decimal retail_price;
+        decimal wholesale_price;
+        decimal special_price;
+        decimal last_price;
+        int quantity;
+        decimal discount;
+        int pack_size;
+        decimal pack_price;
+        DateTime last_updated;
+        bool deleted;        
+
+    }
+}
